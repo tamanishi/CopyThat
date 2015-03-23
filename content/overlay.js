e2c0ca0143c3c2669b916a79415aaca1430998c5
tamanishi <tamanishi@gmail.com>
2015-03-23 15:35:41 +0900

    fix dialog message.

--- content/overlay.js
+++ content/overlay.js
@@ -9,27 +9,31 @@ var CopyThat = {
     var doc = window.content.document;
     var scriptTags = doc.getElementsByTagName("script");
     // alert("scriptTags.length = " + scriptTags.length);
-    var found = false;
+    var found = 0;
+    var result = "";
     for (var i = 0; i < scriptTags.length; i++) {
-        var srcName = scriptTags[i].getAttribute("src");
-        if (srcName != null && ~srcName.indexOf("tracer")) {
-            // alert(srcName);
-            var splitted = srcName.split("?");
-            if (splitted.length > 1) {
-                splitted = splitted[1].split("&");
-                for (var j = 0; j < splitted.length; j++) {
-                    var subSplitted = splitted[j].split("=");
-                    if (subSplitted.length > 1 && subSplitted[0] === "p") {
-                        // alert("found. " + subSplitted[1]);
-                        gClipboardHelper.copyString(subSplitted[1]);
-                        gPrompts.alert(window, title, "found! copied " + splitted[j]);
-                        found = true;
-                    }
-                }
+      var srcName = scriptTags[i].getAttribute("src");
+      if (srcName != null && ~srcName.indexOf("tracer")) {
+        // alert(srcName);
+        var splitted = srcName.split("?");
+        if (splitted.length > 1) {
+          splitted = splitted[1].split("&");
+          for (var j = 0; j < splitted.length; j++) {
+            var subSplitted = splitted[j].split("=");
+            if (subSplitted.length > 1 && subSplitted[0] === "p") {
+            // alert("found. " + subSplitted[1]);
+              result += subSplitted[1];
+              result += "\n";
+              found++;
             }
+          }
         }
+      }
     }
-    if (!found) {
+    if (found > 0) {
+      gClipboardHelper.copyString(result);
+      gPrompts.alert(window, title, "found! " + found + " element(s) copied.");
+    } else {
       gPrompts.alert(window, title, "not found.");
     }
   }
